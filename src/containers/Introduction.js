18fa3b1b91e4c561f5604fd5e634ab9493cae29a
vquine <01010110.v@gmail.com>
2022-03-09 00:00:00 -0500

    create a folder to house container components, create a functional component to introduce users to the application

---
+++ src/containers/Introduction.js
@@ -0,0 +1,12 @@
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+import React from 'React'
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+const Introduction = () => {
+    return (
+        <div>
+            INTRO WILL LIVE HERE
+        </div>
+    )
+}
+export default Introduction;
