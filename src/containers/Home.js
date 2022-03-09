a7e60539f996a0a06c9e7202434222f8cbb61c9c
vquine <01010110.v@gmail.com>
2022-03-09 01:09:54 -0500

    create a container component to house the future home page

---
+++ src/containers/Home.js
@@ -0,0 +1,12 @@
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+import React from 'react'
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+const Home = () => {
+    return (
+        <div>
+            THE HOME PAGE WILL LIVE HERE
+        </div>
+    )
+}
+export default Home;
