8cd80f56c6c47c8612522fefabbad441027fc372
vquine <01010110.v@gmail.com>
2022-03-09 01:12:15 -0500

    navigating functional component created inside of a concerns seperating folder

---
+++ src/components/Navigation.js
@@ -0,0 +1,12 @@
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+import React from 'react'
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+const Navigation = () => {
+    return (
+        <div>
+            A NAV BAR WILL LIVE HERE
+        </div>
+    )
+}
+export default Navigation;
