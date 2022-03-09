a9e919c82b07824e110ec53c2454662089b44501
vquine <01010110.v@gmail.com>
2022-03-09 17:24:11 -0500

    turn intro component into a custom hook function that sets a timer on component mount, and then moves the user to a different route.

--- src/containers/Introduction.js
+++ src/containers/Introduction.js
@@ -1,12 +1,37 @@
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 import React from 'react'
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+import { useEffect } from 'react'
+// REPLACES 'COMPONENTDIDMOUNT' LIFECYCLE
+import { useNavigate } from 'react-router-dom';
+// 'N A V I G A T E' REPLACES 'HISTORY.PUSH'
+// REDIRECTS USER TO ANOTHER PAGE IN APPLICATION
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
-const Introduction = () => {
+// C U S T O M   H O O K   F U N C T I O N
+function Introduction() {
+    
+    // STORE IT TO USE IT
+    let navigate = useNavigate();
+
+    // M Y   F U N C T I O N  TO PUSH A USER TO ANOTHER URL|ROUTE
+    function pushUserHome() {
+        navigate('/loves')
+    }
+
+    // WHEN C O M P O N E N T IS M O U N T E D...
+    useEffect(() => {
+        // MY FUNCTION IS INVOKED AFTER T I M E  (MILLISECONDS) HAS E L A P S E D
+        setTimeout(() => {pushUserHome()}, 1500);
+    })
+
+    // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
     return (
         <div>
-            INTRO WILL LIVE HERE
+            ▀▄▀▄▀▄ I N T R O   L I V E S   H E R E ! ▄▀▄▀▄▀
         </div>
     )
+    // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+    
 }
 export default Introduction;
