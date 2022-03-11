4830cc20ff6aabf995e62229bfd5f806e121149f
vquine <01010110.v@gmail.com>
2022-03-10 22:58:53 -0500

    create some test blog data to refer to

---
+++ src/assets/blogs.js
@@ -0,0 +1,24 @@
+const blogs = [
+
+    {   
+        date: "June 50th, 2525",
+        time: "2:65PM",
+        title: "Blog Test No. 1 TITLE",
+        content: "MESSAGE BODY lives here"
+    },
+
+    {
+        date: "March 31st, 1999",
+        time: "4400 Hours",
+        title: "No. 2, Test Blog, TiTlE",
+        content: "abdefghijklmnopqrstuvwxyz"
+    },
+
+    {
+        date: "00-00-0000",
+        time: "00:00XX",
+        title: "ASDFJKLZXCVBNMQWERTYUIOP",
+        content: "ASDFJKLZXCVBNMQWERTYUIOPASDFJKLZXCVBNMQWERTYUIOPASDFJKLZXCVBNMQWERTYUIOPASDFJKLZXCVBNMQWERTYUIOPASDFJKLZXCVBNMQWERTYUIOPASDFJKLZXCVBNMQWERTYUIOPASDFJKLZXCVBNMQWERTYUIOP"
+    }
+
+]
