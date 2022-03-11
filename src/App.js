6492b37d2a25054bfd587b5990927cbb8d8af5d5
vquine <01010110.v@gmail.com>
2022-03-11 00:37:30 -0500

    app has a wonderful 3rd row of url nesting within to house the blogs container

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 import './App.css'
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 import Introduction from './containers/Introduction'
 import Home from './containers/Home'
 import Code from './containers/Code'
+import Blog from './containers/Blog'
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
 class App extends React.Component {
@@ -20,10 +22,12 @@ class App extends React.Component {
           <Routes>
             <Route path="/" element={<Introduction />} />
 
-            {/* * N E S T E D   R O U T E -  */}
+            {/* * N E S T E D   R O U T E S-  */}
             {/* PARENT WILL REQUIRE 'O U T L E T' COMPONENT FOR CHILDREN TO RENDER */}
             <Route path="loves" element={<Home />} >
-              <Route path="code" element={<Code />} />  
+              <Route path="code" element={<Code />} >  
+                <Route path="blogs" element={<Blog />} />  
+              </Route>
             </Route>
 
           </Routes>
