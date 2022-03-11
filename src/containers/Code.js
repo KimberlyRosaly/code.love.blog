2789ec8c954a61af2570fa90a99c53e6d59cb1a5
vquine <01010110.v@gmail.com>
2022-03-11 03:45:25 -0500

    basic cleanup, moving things around, adding some silly styling to see how elements take up space on the page

--- src/containers/Code.js
+++ src/containers/Code.js
@@ -1,7 +1,7 @@
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 import React from 'react'
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-import { Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom'
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
 // C O D E   P A G E
