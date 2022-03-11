c2a518a0f5a9f12273253d6670d6d205eba3d94c
vquine <01010110.v@gmail.com>
2022-03-10 23:51:43 -0500

    create a nested route for some artistic url wording

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Introduction from './containers/Introduction'
 import Home from './containers/Home'
+import Code from './containers/Code'
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
 class App extends React.Component {
@@ -18,7 +19,13 @@ class App extends React.Component {
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Introduction />} />
-            <Route path="/loves" element={<Home />} />
+
+            {/* * N E S T E D   R O U T E -  */}
+            {/* PARENT WILL REQUIRE 'O U T L E T' COMPONENT FOR CHILDREN TO RENDER */}
+            <Route path="loves" element={<Home />} >
+              <Route path="code" element={<Code />} />  
+            </Route>
+
           </Routes>
         </BrowserRouter>
 
