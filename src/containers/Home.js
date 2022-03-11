b3473e0a0bded00792712364b01f784ffd422d03
vquine <01010110.v@gmail.com>
2022-03-10 23:53:08 -0500

    home page container imports react router v6's 'Outlet' component to allow for children components to render within which is necessary for nested routing

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,6 +1,9 @@
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 import React from 'react'
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+import { Outlet } from 'react-router-dom';
+// REQUIRED FOR NESTED ROUTE PARENT TO RENDER ITS CHILDREN
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 import Navigation from '../components/Navigation';
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
@@ -10,6 +13,9 @@ const Home = () => {
         <div>
 {/* =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */}
             <Navigation />
+{/* =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */}
+{/* ALLOW N E S T E D   R O U T E CHILDREN COMPONENTS TO RENDER */}
+            <Outlet />
 {/* =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */}
         </div>
     )
