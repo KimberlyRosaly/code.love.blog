155058677a7b3e11260ce7e8e7355bb9abc54e43
vquine <01010110.v@gmail.com>
2022-03-10 23:54:09 -0500

    Navigation bar now has a link for a nested route containing the 'Code' page container

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -11,6 +11,8 @@ const Navigation = () => {
             <Link to={'/'}>s t a r t  o v e r</Link>
             <br />
             <Link to={'/loves'}>g o  h o m e</Link>
+            <br />
+            <Link to={'/loves/code'}>g o  c o d e</Link>
         </div>
     )
 }
