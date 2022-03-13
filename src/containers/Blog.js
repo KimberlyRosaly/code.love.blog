789033a5906ff4e610e3e7c81f8c34e6f48651ab
vquine <01010110.v@gmail.com>
2022-03-12 23:56:40 -0500

    play around with state

--- src/containers/Blog.js
+++ src/containers/Blog.js
@@ -1,29 +1,57 @@
-// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 import React from 'react'
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-import { sanitize } from 'dompurify'
-// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-import blogsData from '../assets/blogsData'
+import blogsData from '../assets/blogsData';
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
-// B L O G   P A G E
-const Blog = () => {
-      
-    return (
-        <div>
+class Blog extends React.Component {
+
+    state = {
+        blogs: [...blogsData],
+        justClicked: 0
+    }
+
+    handleClick = (event, b) => {
+        // console.log(this.state.blogs.find((b) => b.id == 1))
+        // console.log(e.target.dataset.bloggie) //<==this sometimes works
+
+        // this.setState({justClicked: e.target.dataset.bloggie})
+
+        console.log("THIS JUSTCLICKED:", this.state.justClicked)
+        console.log("THIS JUSTCLICKED:", b)
+        this.setState({justClicked: b})
+        console.log("THIS.JUSTCLICKED: (after setState)", this.state.justClicked)
+
+    }
+
+    
+    render() {
+
+        // console.log("STATE.BLOGS:", this.state.blogs)
+
+      return (
+        <div className='b-container'>
 {/* =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */}
             BLOG CONTAINER LIVES HERE ♥
-{/* =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */}
-            {blogsData.map((b) => 
-                <div>
-                    <h1>{b.title}</h1>
-                    <h3>{b.date} | {b.time}</h3>                    
-                    <div dangerouslySetInnerHTML={{ __html: sanitize(b.content) }} />
-                    <br />
-                </div>
-            )}
-{/* =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */}
+{/* =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */}    
+
+
+
+
+
+        <div >            
+                    <ul>                        
+                        {blogsData.map( (b) => 
+                            <li key={b.id} className='b-dropdown-item' onClick={(event) => this.handleClick(event, b.id)} >
+                                <h3>BLOGPOST # {b.id}</h3>
+                                {b.title}
+                            </li> )}                        
+                    </ul>
+                
         </div>
-    )
-}
-export default Blog;
+
+
+        </div>
+      )
+    }
+  }
+  export default Blog;
