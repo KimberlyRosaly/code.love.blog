19df78f7d3d59dfd2e96acc39bc577a2028103d3
vquine <01010110.v@gmail.com>
2022-03-14 20:58:48 -0400

    create a functional component to render a blog's content to the page from a prop handing in from the parent

---
+++ src/components/BlogPost.js
@@ -0,0 +1,13 @@
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+import React from 'react'
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+// F U N C T I O N A L   C O M P O N E N T
+const BlogPost = (props) => {
+    return (
+        <div>
+            {/* A SINGLE BLOG'S CONTENT WILL LIVE HERE */}
+        </div>
+    )
+}
+export default BlogPost;
