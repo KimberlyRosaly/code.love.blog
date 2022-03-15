6b14f99d076674154d53dd985f82f9c55ed4fb8a
vquine <01010110.v@gmail.com>
2022-03-14 20:03:56 -0400

    blank layout to feature desired behavior to render components, state, and events

--- src/containers/Blog.js
+++ src/containers/Blog.js
@@ -3,55 +3,35 @@ import React from 'react'
 import blogsData from '../assets/blogsData';
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
+// B L O G   P A G E
 class Blog extends React.Component {
 
     state = {
-        blogs: [...blogsData],
-        justClicked: 0
+      // INITIAL VALUES UPON LANDING
+      blogSelected: false
+    }  
+
+    handleClick = (event, blogID) => {
+      
     }
-
-    handleClick = (event, b) => {
-        // console.log(this.state.blogs.find((b) => b.id == 1))
-        // console.log(e.target.dataset.bloggie) //<==this sometimes works
-
-        // this.setState({justClicked: e.target.dataset.bloggie})
-
-        console.log("THIS JUSTCLICKED:", this.state.justClicked)
-        console.log("THIS JUSTCLICKED:", b)
-        this.setState({justClicked: b})
-        console.log("THIS.JUSTCLICKED: (after setState)", this.state.justClicked)
-
-    }
-
     
+    // CONDITIONALLY RENDER WHAT SHOWS UP INSIDE THE BLOG PAGE CONTAINER
     render() {
-
-        // console.log("STATE.BLOGS:", this.state.blogs)
-
-      return (
-        <div className='b-container'>
-{/* =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */}
-            BLOG CONTAINER LIVES HERE ♥
-{/* =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */}    
-
-
-
-
-
-        <div >            
-                    <ul>                        
-                        {blogsData.map( (b) => 
-                            <li key={b.id} className='b-dropdown-item' onClick={(event) => this.handleClick(event, b.id)} >
-                                <h3>BLOGPOST # {b.id}</h3>
-                                {b.title}
-                            </li> )}                        
-                    </ul>
-                
-        </div>
-
-
-        </div>
-      )
+      // IF STATE CONTAINS A PROPERTY HOLDING ONTO A BLOG SELECTION...
+      if (this.state.blogSelected) {
+        return (
+          <div className='b-container'>
+            <div>{/* BLOG MENU FEATURING TITLES OF POSTS */}</div>
+            <div>{/* BLOG CONTENT OF A USER-SELECTED POST */}</div>
+          </div>
+      )}
+      // STATE HAS NOTHING COLLECTED | THE BEAUTIFUL DEFAULT LANDING
+      else {
+        return (
+          <div className='b-container'>
+            <div>{/* BLOG MENU FEATURING TITLES OF POSTS */}</div>
+          </div>
+      )}        
     }
-  }
-  export default Blog;
+}
+export default Blog;
