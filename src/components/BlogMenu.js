d03fccb2c02f493d0281a20e979b3dffc94b7d8e
vquine <01010110.v@gmail.com>
2022-03-14 20:37:33 -0400

    create a functional component containing a spiffy, clickable listing of blog title props passed from the parent page container

---
+++ src/components/BlogMenu.js
@@ -0,0 +1,25 @@
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+import React from 'react'
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+// F U N C T I O N A L   C O M P O N E N T
+const BlogMenu = (props) => {
+    return (
+        <div>
+            <ul>                        
+                {props.blogs.map( (b) => 
+                    <li 
+                        key={b.id} 
+                        className='b-dropdown-item' 
+                        onClick={(event) => props.handleClick(event, b.id)} >
+                        {/* 'HANDLECLICK' IS A CLICK EVENT HANDLING FUNCTION ACCESSIBLE VIA PARENT - FUNCTION PASSED DOWN AS PROP */}
+
+                        <h3>BLOGPOST # {b.id}</h3>
+                        <p>{b.title}</p>
+
+                    </li> )}                        
+            </ul>
+        </div>
+    )
+}
+export default BlogMenu;
