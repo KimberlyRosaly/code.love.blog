40d4ea7413fe3561f9c5275cd5146a91eae09ad4
vquine <01010110.v@gmail.com>
2022-03-15 18:04:16 -0400

    add an image property to my blog data objects with test files

--- src/assets/blogsData.js
+++ src/assets/blogsData.js
@@ -4,6 +4,7 @@ const blogsData = [
         date: "June 50th, 2525",
         time: "2:65PM",
         title: "Blog Test No. 1 TITLE",
+        image: "https://previews.dropbox.com/p/thumb/ABdZ82kwS0N0buFn2uOM23D8wP4MsNcDMXCtMdiF8cKNgDjL-n9UbNLJjla18wVETUCyqYdwnf-YA9kCkWUr-ppf3lqNTH_toX1fE1gWCQBNZ3QnMXmxU2recrx5EpFy0IISY2LzqDqk36LgImhpgLgBV3tjBshtw7bqkUdCvKsM_tp2qJNUyjvawyktsbhw_8z5c9SZBaZ6k3g9ex6CNcZf_btCw5gw1sCLRHHYFVz1-3gNsyRYHnBpAppJf7B1KxkYVNHkLe5JiKsOntujMvTRWJizcAIX140XG3pqTBZa7hmPfVahbokLk2093V8yAWxnogXZata28CDot04Gv4EMkuvpFywzfxKIqGH45Jv7XPw0v1_6sY9Gh7nZSFEEiAQ/p.png",
         content: "MESSAGE BODY lives here"
     },
 
@@ -12,6 +13,7 @@ const blogsData = [
         date: "March 31st, 1999",
         time: "4400 Hours",
         title: "No. 2, Test Blog, TiTlE",
+        image: "https://previews.dropbox.com/p/thumb/ABf19lBKdidl0vzZeBdTeQBs3mzf27W4F6y7-23vyAQR2TNhu-OhpCI-hK4Crithhw45k1Cd7AtZOT2X0DR4OkqKnA7l8w6d4IZ81AsbtAhUfs0RHxyFGcp_r9pMI7VfpHI1TzBYkP-wtTro5ebt_F-3Vw_EC8YTKmvn43RTzKrGhCc6z-kZQW27bnjsWXqv1qmGAvb1QMv_CAaRj4DtBYroKJJsRo8GWp8OxodrpXW6NsZgj0ZOhBFta2YCCRoDsyPKbU5tkf3i1iiT6UXUqvY368p069YWnG4_KsfJDeWSv7FDFI3DSnQtom5OclNNQV0DtgaRip3nn9bZGiGBoC1vqdc-591MTXIdF50l1ZsYO9bisi9j1NoXv-dfN6m7ZjQ/p.png",
         content: "abdefghijklmnopqrstuvwxyz"
     },
 
@@ -20,6 +22,7 @@ const blogsData = [
         date: "00-00-0000",
         time: "00:00XX",
         title: "ASDFJKLZXCVBNMQWERTYUIOP",
+        image: "https://previews.dropbox.com/p/thumb/ABdZ82kwS0N0buFn2uOM23D8wP4MsNcDMXCtMdiF8cKNgDjL-n9UbNLJjla18wVETUCyqYdwnf-YA9kCkWUr-ppf3lqNTH_toX1fE1gWCQBNZ3QnMXmxU2recrx5EpFy0IISY2LzqDqk36LgImhpgLgBV3tjBshtw7bqkUdCvKsM_tp2qJNUyjvawyktsbhw_8z5c9SZBaZ6k3g9ex6CNcZf_btCw5gw1sCLRHHYFVz1-3gNsyRYHnBpAppJf7B1KxkYVNHkLe5JiKsOntujMvTRWJizcAIX140XG3pqTBZa7hmPfVahbokLk2093V8yAWxnogXZata28CDot04Gv4EMkuvpFywzfxKIqGH45Jv7XPw0v1_6sY9Gh7nZSFEEiAQ/p.png",
         content: "this is a test <h1> this is <hr /> another test </h1>"
     }
 ]
