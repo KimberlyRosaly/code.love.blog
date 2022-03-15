86d972cd2cfb4823b0708e25c4296b55706d5a3f
vquine <01010110.v@gmail.com>
2022-03-14 21:19:10 -0400

    HTML injection santization implemented to allow blog content to have beautiful HTML markup rendered too

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -1,13 +1,17 @@
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 import React from 'react'
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+import { sanitize } from 'dompurify'
+// PREVENT CROSS-SITE SCRIPTING (XSS) INJECTION ATTACKS
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
 // F U N C T I O N A L   C O M P O N E N T
 const BlogPost = (props) => {
     return (
-        <div>
+        <div dangerouslySetInnerHTML={{ __html: sanitize(props.blog.content) }}>
             {/* A SINGLE BLOG'S CONTENT WILL LIVE HERE */}
-            {props.blog.content}
+            {/* HTML INJECTION SANITIZED TO HELP PREVENT XSS ATTACKS */}
+            {/* TEXT SET DANGEROUSLY BECAUSE OF DESIRED HTML FORMATTING WANTING TO BE RENDERED */}
         </div>
     )
 }
