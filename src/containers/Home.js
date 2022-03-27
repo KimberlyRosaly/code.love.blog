845ebf6908ec3bdd12e590ff33cf6412fb94eebe
vquine <01010110.v@gmail.com>
2022-03-27 02:15:54 -0400

    start the whole 'choose your adventure' aspect, adding some buttons to allow two paths of either code stuff or art things

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -3,6 +3,7 @@ import React from 'react'
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 import { Outlet } from 'react-router-dom';
 // REQUIRED FOR NESTED ROUTE PARENT TO RENDER ITS CHILDREN
+import { Link } from 'react-router-dom';
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 import Navigation from '../components/Navigation';
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
@@ -14,7 +15,15 @@ const Home = () => {
 {/* =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */}
             <Navigation />
 {/* =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */}
-        <img className="home-image" src="https://www.dropbox.com/s/csy354hzgirqmgs/KimberlyRosaly.png?raw=1" />
+        <img className="home-image" src="" />
+{/* =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */}
+
+        <div className='home-choices' style={{backgroundImage: `url(https://www.dropbox.com/s/csy354hzgirqmgs/KimberlyRosaly.png?raw=1)`}}>
+            <Link to={'/loves/code'}>
+                <button>C O D E</button>
+            </Link>
+                <button>A R T</button>
+        </div>
 {/* =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */}
 {/* ALLOW N E S T E D   R O U T E CHILDREN COMPONENTS TO RENDER */}
             <Outlet />
