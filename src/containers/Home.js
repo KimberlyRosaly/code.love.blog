f7132ef2b6155d965dccb8dca7fded274934b777
vquine <01010110.v@gmail.com>
2022-03-28 00:16:43 -0400

    when clicking on a button to choose your adventure, the beginning of it, anyway, the DOM will scroll the user down so that the rendered component is put into view

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,15 +1,33 @@
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-import React from 'react'
+import React from 'react';
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 import { Outlet } from 'react-router-dom';
 // REQUIRED FOR NESTED ROUTE PARENT TO RENDER ITS CHILDREN
 import { Link } from 'react-router-dom';
+// CLIENT-SIDE ROUTING - PUSHES USER WHERE SPECIFICED
+import { useEffect } from 'react';
+// ALLOW LIFECYCLE ACCESS WITHIN A HOOK
+import { useRef } from 'react';
+// ACCESS REFERENCES ON THE DOM WITHIN A HOOK
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 import Navigation from '../components/Navigation';
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
+
 // H O M E  P A G E
 const Home = () => {
+
+    
+
+    // SET AN INITIAL, DEFAULT VALUE ON OUR DOM REFERENCING METHOD TO USE LATER
+    const dummyDiv = useRef(null)
+    // 
+    const scrollToBottom = () => {
+        dummyDiv.current?.scrollIntoView({ behavior: "smooth" })
+    }
+
+    useEffect(() => { scrollToBottom() } )
+
     return (
         <div>
 {/* =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */}
@@ -18,17 +36,24 @@ const Home = () => {
         
 {/* =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */}
         <div className='home-choices' style={{backgroundImage: `url(https://www.dropbox.com/s/csy354hzgirqmgs/KimberlyRosaly.png?raw=1)`}}>
+           
             <Link to={'/loves/code'}>
-                <button className='h-c-1'>C O D E</button>
+                <button className='h-c-1'>
+                    C O D E
+                </button>
             </Link>
+           
             <Link to={'/loves'}>
-                <button className='h-c-2'>A R T</button>
+                <button className='h-c-2'>
+                    A R T
+                </button>
             </Link>
         </div>
 {/* =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */}
 {/* ALLOW N E S T E D   R O U T E CHILDREN COMPONENTS TO RENDER */}
             <Outlet />
 {/* =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */}
+            <div ref={dummyDiv}></div>
         </div>
     )
 }
