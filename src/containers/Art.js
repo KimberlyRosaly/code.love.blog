0b5d809d63281edae898d9a5e63f0bf7d7304eaf
vquine <01010110.v@gmail.com>
2022-03-31 23:30:30 -0400

    create an art container to house my art related content components

---
+++ src/containers/Art.js
@@ -0,0 +1,11 @@
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+import React from 'react'
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+// A R T   P A G E
+const Art = () => {
+    return (
+        <div>ART WILL LIVE HERE!!! ♥ </div>
+    )
+}
+export default Art;
