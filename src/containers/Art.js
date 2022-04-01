26e870cede4db1a765cddfb4953a699187fa2441
vquine <01010110.v@gmail.com>
2022-03-31 23:38:56 -0400

    Art container will now scroll the user to the bottom of the page, I chose to fragment the rendering this time because the dummy div should be unrelated to the container div itself

--- src/containers/Art.js
+++ src/containers/Art.js
@@ -1,11 +1,30 @@
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 import React from 'react'
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+import { useEffect } from 'react';
+import { useRef } from 'react';
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
 
 // A R T   P A G E
 const Art = () => {
-    return (
-        <div>ART WILL LIVE HERE!!! ♥ </div>
-    )
+// = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+const dummyDiv = useRef(null)
+const scrollToBottom = () => { dummyDiv.current?.scrollIntoView({ behavior: "smooth" }) }
+useEffect( () => {scrollToBottom()} )
+// = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+
+    return (<>
+        <div>
+            
+            ART WILL LIVE HERE!!! ♥ 
+
+        </div>
+
+        {/* =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= */}
+        <div ref={dummyDiv} />
+        {/* =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= */}
+        
+    </>)
 }
 export default Art;
