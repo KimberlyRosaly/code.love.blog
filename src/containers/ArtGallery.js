b285579c4550a5a31abab706fb527d6dbcebff1a
vquine <01010110.v@gmail.com>
2022-04-02 01:16:42 -0400

    create the parent component to contain children that iterate and render

---
+++ src/containers/ArtGallery.js
@@ -0,0 +1,28 @@
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+import React from 'react'
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+import { useEffect } from 'react';
+import { useRef } from 'react';
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+import artData from '../assets/artData';
+import Artworks from '../components/Artworks';
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+
+// A R T W O R K S   CONTAINER  C O M P O N E N T
+const ArtGallery = () => {
+// = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+
+    return (<>
+        <div>
+            
+            ART GALLERY WILL LIVE HERE!!! ♥ 
+
+            <Artworks />
+
+        </div>
+
+  
+    </>)
+}
+export default ArtGallery;
