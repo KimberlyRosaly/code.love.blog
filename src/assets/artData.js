6a7d7c4cf052a727c667dd3649749149b3cd7673
vquine <01010110.v@gmail.com>
2022-04-02 00:07:02 -0400

    add some image urls  that are hosted somewhere else of artwork

--- src/assets/artData.js
+++ src/assets/artData.js
@@ -1,7 +1,8 @@
-const artData = [{
-    image_0: "",
-    image_1: "",
-    image_2: "",
-    image_3: "",
-}]
+const artData = [
+    {id: 1, image_url: "https://live.staticflickr.com/65535/51642757555_78890b6264_z.jpg"},
+    {id: 2, image_url: "https://www.dropbox.com/s/uwdz33es55qozr4/IMG_1432.jpg?raw=1"},
+    {id: 3, image_url: "https://www.dropbox.com/s/gxuwwvcoja6h6i6/IMG_1430%20%282%29.jpg?raw=1"},
+    {id: 4, image_url: "https://www.dropbox.com/s/wnn2hgh26r3ve2c/FancyCatPrince_kimrosaly_com.png?raw=1"},
+    {id: 5, image_url: "https://www.dropbox.com/s/4bgvsn2c1xxj7nk/IMG_1431%20%282%29.jpg?raw=1"}
+]
 export default artData;
