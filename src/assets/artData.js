123114fb873ca42853e7caf713bdbc6f9a2fa7b4
vquine <01010110.v@gmail.com>
2022-04-04 05:44:06 -0400

    fill out my art gallery sample images data

--- src/assets/artData.js
+++ src/assets/artData.js
@@ -3,6 +3,9 @@ const artData = [
     {id: 2, image_url: "https://www.dropbox.com/s/uwdz33es55qozr4/IMG_1432.jpg?raw=1"},
     {id: 3, image_url: "https://www.dropbox.com/s/gxuwwvcoja6h6i6/IMG_1430%20%282%29.jpg?raw=1"},
     {id: 4, image_url: "https://www.dropbox.com/s/wnn2hgh26r3ve2c/FancyCatPrince_kimrosaly_com.png?raw=1"},
-    {id: 5, image_url: "https://www.dropbox.com/s/4bgvsn2c1xxj7nk/IMG_1431%20%282%29.jpg?raw=1"}
+    {id: 5, image_url: "https://www.dropbox.com/s/4bgvsn2c1xxj7nk/IMG_1431%20%282%29.jpg?raw=1"},
+    {id: 6, image_url: "https://www.dropbox.com/s/8uudh3c3eykcjkm/Untitled_Artwork.png?raw=1"},
+    {id: 6, image_url: "https://www.dropbox.com/s/mq22gb3ibcmt9a5/Umm...png?raw=1"},
+    {id: 6, image_url: "https://www.dropbox.com/s/wu3e1zde9nhh1yt/Untitled_Artwork%20%281%29.png?raw=1"}
 ]
 export default artData;
