d1c4704e9bc9f0d0cc48deb6d917b96f721d523e
vquine <01010110.v@gmail.com>
2022-04-14 23:55:52 -0400

    create a container component to house the art gallery's individual artwork image's show page

---
+++ src/containers/ArtShow.js
@@ -0,0 +1,16 @@
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+import React from 'react'
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+
+
+// A R T  ♥  S H O W
+const ArtShow = () => {
+// = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+
+
+    return (<>
+    <p>ART SHOW PAGE CONTAINER LIVES HERE</p> 
+    </>)
+}
+export default ArtShow;
