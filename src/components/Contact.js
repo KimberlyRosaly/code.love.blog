12c47f2059e71305227d9345ff41e94a3bf896c5
vquine <01010110.v@gmail.com>
2022-04-30 03:51:14 -0400

    create a contact section for a page, has class name on parent element returned for CSS use

---
+++ src/components/Contact.js
@@ -0,0 +1,25 @@
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+import React from 'react'
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+
+
+// c o n t a c t  i n f o     C O M P O N E N T
+const Contact = () => {
+// = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+
+    return (
+        <div className='contact-container'>
+
+            <div>             
+                <h1>C O N T A C T</h1>
+            </div>
+
+            <div>
+                <h1>▼・ᴥ・▼</h1>
+            </div>
+
+        </div>  
+    )
+}
+export default Contact;
