bead083c8d5bfbe6c9ea8ebcf9f4ec5c508e353c
vquine <01010110.v@gmail.com>
2022-05-01 01:43:48 -0400

    display hardcoded information and links to external resources

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,24 +1,33 @@
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 import React from 'react'
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-
+// import { useState, useEffect, useRef } from 'react'
+// A D V A N C E D  D E S T R U C T U R I N G
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
 
 // c o n t a c t  i n f o     C O M P O N E N T
 const Contact = () => {
+// = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = 
 // = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 
     return (
         <div className='contact-container'>
-
-            <div>             
-                <h1>C O N T A C T</h1>
+            
+            <div>
+                <h1>CONTACT ME</h1>
             </div>
 
             <div>
-                <h1>▼・ᴥ・▼</h1>
+                <a href='https://www.linkedin.com/in/kimberly-rosaly/'>
+                    <h1>LINKEDIN</h1>
+                </a>
+                <a href='https://github.com/KimberlyRosaly/'>
+                    <h1>GITHUB</h1>
+                </a>                
+                <h1>kra.dev.cc@gmail.com</h1>
             </div>
-
+            
         </div>  
     )
 }
